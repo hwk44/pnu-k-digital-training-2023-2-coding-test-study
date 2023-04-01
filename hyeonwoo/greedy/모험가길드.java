1adaaf463c0cd0b9325acf890ecc4b013a684f1c
hwk44 <peos0n@kakao.com>
2023-04-01 10:03:38 +0900

    4/1 greedy

---
+++ hyeonwoo/greedy/모험가길드.java
@@ -0,0 +1,16 @@
+package greedy;
+
+import java.util.Scanner;
+
+public class 모험가길드 {
+    /*
+    * n 명 모험가 공포도 x
+    * 공포도 x 인 모험가는 x명 이상 그루핑 해야 모험할수 있음
+    * 최대 몇개 그룹?*/
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+
+    }
+}
