567b750f181fb24855a584527d683c9d75425c15
hwk44 <peos0n@kakao.com>
2023-05-02 19:47:03 +0900

    0502

---
+++ hyeonwoo/백준/Wee02_DFS_BFS/_2178_미로탐색.java
@@ -0,0 +1,22 @@
+package 백준.Wee02_DFS_BFS;
+
+import javax.management.MBeanAttributeInfo;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class _2178_미로탐색 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        int[][] graph;
+
+    }
+}
--- hyeonwoo/백준/Wee02_DFS_BFS/_24479_깊이우선탐색.java
+++ hyeonwoo/백준/Wee02_DFS_BFS/_24479_깊이우선탐색.java
@@ -1,11 +1,27 @@
 package 백준.Wee02_DFS_BFS;
 
+
 import java.io.*;
 import java.util.*;
 
 public class _24479_깊이우선탐색 {
+    static boolean[] vis;
+    static List<List<Integer>> graph;
+    static int[] answer;
+    static  int order = 1;
+    public static void dfs(int s) {
+
+        vis[s] = true;
+        answer[s] = order++;
+
+        for (int i = 0; i < graph.get(s).size(); i++) {
+            int n = graph.get(s).get(i);
+            if (!vis[n]) dfs(n);
+        }
+    }
     public static void main(String[] args) throws IOException {
         Scanner sc = new Scanner(System.in);
+
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
@@ -17,66 +33,51 @@ public class _24479_깊이우선탐색 {
 //        int start = sc.nextInt();
         int start = Integer.parseInt(st.nextToken());
 
-
+        vis = new boolean[n+1];
         int[] visited = new int[n+1]; // 정점 방문확인
 //        int[][] gragh = new int[n][n]; // 간선 정보 2차원 배열
 
-        List<List<Integer>> gragh = new ArrayList<>();
+//        List<List<Integer>>
+        graph = new ArrayList<>();
 
         for (int i = 0; i < n+1; i++) {
-            gragh.add(new ArrayList<>());
+            graph.add(new ArrayList<>());
         }
         for (int i = 0; i <m; i++) {
             st = new StringTokenizer(br.readLine());
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
-//            int a = sc.nextInt();
-//            int b = sc.nextInt();
 
-            gragh.get(a).add(b);
-            gragh.get(b).add(a);
-//            gragh[a-1][b-1] = gragh[b-1][a-1] = 1;
+            graph.get(a).add(b);
+            graph.get(b).add(a);
         }
 
-//        for (int i = 0; i < n+1; i++) {
-//
-//            System.out.println(gragh.get(i));
-//        }
+        // 리스트 별로 내림차순 정렬
+        for (List list : graph) {
+            Collections.sort(list, Collections.reverseOrder());
+        }
+        answer = new int[n+1];
+
+        dfs(start);
+
+        Stack<Integer> stack = new Stack<>();
+        stack.add(start);
 
 
-//        Queue<Integer> q = new LinkedList<>();
-        PriorityQueue<Integer> q = new PriorityQueue<>(new Comparator<Integer>() {
-            @Override
-            public int compare(Integer o1, Integer o2) {
-                return Integer.compare(o1,o2);
-            }
-        });
-        q.offer(start);
-
-
-        int[] answer = new int[n+1];
         StringBuilder sb = new StringBuilder();
         int cnt =0;
 
 
-        while (!q.isEmpty()) {
-            // 큐에서 제거한 숫자저장
-            int a = q.poll(); // 정점을 큐에서 빼냄
-            // 방문처리
-            visited[a] = 1;
-//            System.out.println("빼낸 숫자 : " + a);
-            if (a >= start ) answer[a] = ++cnt;
-//            sb.append(a + " ");
+//        while (!stack.isEmpty()) {
+//            int a = stack.pop();
+//            // 방문처리
+//            visited[a] = 1;
 //            answer[a] = ++cnt;
-
-            for (int e : gragh.get(a)) {
-                // 정점을 큐에 담고 방문처리
-//                visited[i+1] = 1;
-                if (visited[e] ==0 && !q.contains(e) && e >= start) q.offer(e);
-            }
-
-
-        }
+//            for (int e : graph.get(a)) {
+//                if (visited[e] == 0 && !stack.contains(e) ) stack.add(e);
+//            }
+//        }
+//
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         for (int i = 1; i < n+1; i++) {
 //            System.out.println(answer[i]);
